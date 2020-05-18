5e4b08e23c36f41cedb129fed27e4449703afcd6
irynailina <irynailina@gmail.com>
2020-05-18 15:23:45 +0300

    Updates

---
+++ precache-manifest.34d440e245543478c2dafa65dc667160.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e26dc09713c7b369aad829c67231d7b9",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "541dd985ace7755acd46",
+    "url": "/goit-react-hw-05-phonebook/static/css/2.358ce93d.chunk.css"
+  },
+  {
+    "revision": "a5c182da075cda8dee3d",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.a06eb227.chunk.css"
+  },
+  {
+    "revision": "541dd985ace7755acd46",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.21b6208a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.21b6208a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5c182da075cda8dee3d",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.f38347de.chunk.js"
+  },
+  {
+    "revision": "19a5bc77f9379866c4ba",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.025abf9b.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/goit-react-hw-05-phonebook/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/goit-react-hw-05-phonebook/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/goit-react-hw-05-phonebook/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/goit-react-hw-05-phonebook/static/media/notification.c0d3c94c.eot"
+  }
+]);
